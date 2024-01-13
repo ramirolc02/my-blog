16a56e9e55c3c0aa711240ecf2a52cd5260678b8
Rama Lopez Cento <ramirolopezcento@icloud.com>
2024-01-14 00:36:24 +0100

    update image src

--- app/components/ProfilePic.tsx
+++ app/components/ProfilePic.tsx
@@ -5,7 +5,7 @@ export default function ProfilePic() {
     <section className="w-full mx-auto">
       <Image
         className="border-4 border-black dark:border-slate-500 drop-shadow-xl shadow-black rounded-full mx-auto mt-8"
-        src="/images/perfil.png"
+        src="/images/Perfil.png"
         width={200}
         height={200}
         alt="Ramiro Lopez Cento"
