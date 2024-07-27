880ab0b2e370871cdfdbe1f735bb345e3bb14a94
Rama Lopez Cento <ramirolopezcento@icloud.com>
2024-07-27 18:50:25 +0200

    clean landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,13 @@
 import Posts from "./components/Posts"
-import ProfilePic from "./components/ProfilePic"
 export const revalidate = 86400
 
 export default function Home() {
   return (
     <div className="mx-auto">
-      <ProfilePic />
+      {/* <ProfilePic /> */}
       <p className="mt-12 mb-12 text-3xl text-center dark:text-white">
         <span className="whitespace-nowrap">
-          <span className="font-bold"> To-read </span>
+          <span className="font-bold">Ramiro Lopez Cento´s Website</span>
         </span>
       </p>
       <Posts />
