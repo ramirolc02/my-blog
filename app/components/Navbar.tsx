b33640c99d1ddc04aefd94d4215d0f1c47b33469
Rama Lopez Cento <ramirolopezcento@icloud.com>
2024-07-29 22:32:37 +0200

    navbar change

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,13 +1,12 @@
-
-import Link from "next/link";
-import { FaGithub, FaLinkedin } from "react-icons/fa";
+import Link from "next/link"
+import { FaGithub, FaLinkedin } from "react-icons/fa"
 
 export default function Navbar() {
   return (
     <nav className="bg-slate-600 p-4 sticky top-0 drop-shadow-xl z-10">
       <div className=" md:px-6 prose prose-xl mx-auto flex justify-between flex-col sm:flex-row">
         <Link href="/" className="text-white/90 no-underline hover:text-white">
-          Ramiro Lopez Cento
+          Blogs
         </Link>
         <div className="flex flex-row justify-center sm:justify-evenly align-middle gap-4 text-white text-4xl lg:text-5xl space-x-2">
           <Link
@@ -25,5 +24,5 @@ export default function Navbar() {
         </div>
       </div>
     </nav>
-  );
+  )
 }
