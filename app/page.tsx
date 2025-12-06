704d404177d9dee568053b0805fbf5f1a34852c2
Ramiro Lopez Cento <ramirolopezcento@icloud.com>
2025-12-06 19:07:13 +0100

    feat/compound-interest

--- app/page.tsx
+++ app/page.tsx
@@ -8,14 +8,44 @@ export default function Home() {
     <div className="container mx-auto px-4 py-8 sm:py-12 max-w-7xl overflow-hidden">
       <AnimatedWrapper>
         <header className="text-center mb-16">
-          <h1 className="text-4xl sm:text-5xl lg:text-6xl font-bold tracking-tight mb-6 bg-gradient-to-r from-foreground to-foreground/70 bg-clip-text text-transparent">
-            Ramiro Lopez Cento&apos;s Website
-          </h1>
-          <p className="text-xl text-muted-foreground max-w-3xl mx-auto leading-relaxed">
-            Argentinian guy living in Spain. Curiously learning about stuff including tech, sports, health, investing, guitar, and more. Not great at any of them ... yet.
-          </p>
+          <h3 className="text-4xl sm:text-5xl lg:text-6xl font-bold tracking-tight mb-6 bg-gradient-to-r from-foreground to-foreground/70 bg-clip-text text-transparent">
+            Ramiro Lopez Cento
+          </h3>
+          <div className="flex flex-col items-center">
+            <span className="text-xl text-muted-foreground text-center max-w-3xl leading-relaxed">
+              🇦🇷 🇪🇸
+            </span>
+            <span className="text-xl text-muted-foreground text-center max-w-3xl leading-relaxed">
+              Curious about tech, sports, health, investing and guitar.
+            </span>
+            <span className="text-xl text-muted-foreground text-center max-w-3xl leading-relaxed">
+              Computer Engineering & AI Master’s Graduate (
+              <a
+                href="https://www.upm.es/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="underline hover:text-foreground transition-colors"
+              >
+                UPM
+              </a>
+              ).
+            </span>
+            <span className="text-xl text-muted-foreground text-center max-w-3xl leading-relaxed">
+
+              <a
+                href="/compound-interest"
+                rel="noopener noreferrer"
+                className="underline hover:text-foreground transition-colors"
+              >
+                Compound interest enthusiast.
+              </a>
+            </span>
+
+          </div>
         </header>
       </AnimatedWrapper>
+
+
 
       <Posts />
     </div>
